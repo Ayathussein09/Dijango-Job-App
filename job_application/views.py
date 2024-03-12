f5c3a81038fa6d35cce165e501929c8ffd3a0b20
ayathusssein <ayat.hussein@madkour.com.eg>
2024-03-12 09:24:05 +0200

    Initial commit

---
+++ job_application/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
+
+
+# Create your views here.
+def index(request):
+
+    if request.method == 'POST':
+        # print("TTTTest")
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date,
+                                occupation=occupation)
+            messages.success(request, "Form Submitted Successfully!")
+            message_body = f"A new job application was submitted. Thank You {first_name} {last_name}"
+            email_message = EmailMessage("Form Submission Confirmation", message_body, to=[email])
+            email_message.send()
+    return render(request, "index.html")
+    return  re
+
+
+def about(request):
+    return render(request, 'about.html')
